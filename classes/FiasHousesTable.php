f2dfe646c69a457083ecf6e36fd9aa0ae76638a4
Mikhail S.A <mikhail.a.sidorov@gmail.com>
2020-12-05 00:36:59 +0300

    update houses table

--- classes/FiasHousesTable.php
+++ classes/FiasHousesTable.php
@@ -41,30 +41,35 @@ class FiasHousesTable implements TableInfoInterface
 
         return [
             "
-                create table if not exists $tableName
+                create table if not exists ${tableName}
                 (
                     aoguid varchar(36) null,
                     buildnum varchar(10) null,
                     eststatus double null,
                     estname varchar(80) null,
-                    houseguid varchar(36) not null
-                        primary key,
+                    houseguid varchar(36) not null,
                     houseid varchar(36) null,
                     housenum varchar(20) null,
                     statstatus double null,
                     postalcode varchar(6) null,
                     strucnum varchar(10) null,
                     strstatus double null,
-                    strname varchar(80) null,
-                    constraint fias_houses_pk_2
-                        unique (houseid)
+                    strname varchar(80) null
                 )
                 collate=utf8_unicode_ci;
             ",
             "
-                create index fias_houses_aoguid_index
-                    on $tableName (aoguid);
+                create index fias_houses_aoguid_index2
+	            ${tableName} (aoguid);
+	        ",
             "
+                create index fias_houses_houseguid_index
+	            ${tableName} (houseguid);
+            ",
+            "
+                create index fias_houses_housenum_index
+            	${tableName} (housenum);
+	        "
         ];
     }
 
